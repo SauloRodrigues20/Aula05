38987f354592e5ccc726e06e878f9a20b8a29d06
SauloRodrigues20 <130412074+SauloRodrigues20@users.noreply.github.com>
2025-05-06 00:20:45 +0000

    Aula 05/05/2025

---
+++ Spring2/model/Pessoa.java
@@ -0,0 +1,7 @@
+package com.example.projeto.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
